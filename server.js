b90ebfd69be60bebea23a7886fb798dfaf09c3b6
Tuan Tran <tuan.tran@crystal-techs.com>
2019-04-05 18:47:29 +0700

    #EXPLORER-003 swagger api-docs

--- server.js
+++ server.js
@@ -4,10 +4,15 @@ const port = 3000
 
 var bodyParser = require('body-parser')
 
+var swaggerUi = require('swagger-ui-express'),
+    swaggerDocument = require('./swagger.json');
+
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 app.use('/api', require('./api'));
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use((req, res) => res.status(404).send({url: req.originalUrl + ' Not Found'}) )
 
